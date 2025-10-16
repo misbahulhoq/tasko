8989cb1693b68ebaf8eb9e171a56800371d9273e
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2025-10-16 15:17:11 +0600

    updateSubscribeUser logic

--- src/utils/subscribeUser.ts
+++ src/utils/subscribeUser.ts
@@ -1,5 +1,16 @@
 import { baseUrl } from "@/redux/features/api/baseApi";
 
+function urlBase64ToUint8Array(base64String: string) {
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
 export async function subscribeUser(email: string) {
   if (!("serviceWorker" in navigator)) {
     alert("Service Workers are not supported in this browser.");
@@ -8,11 +19,14 @@ export async function subscribeUser(email: string) {
   const registration = await navigator.serviceWorker.register("/sw.js");
   const subscription = await registration.pushManager.subscribe({
     userVisibleOnly: true,
-    applicationServerKey: process.env.NEXT_PUBLIC_VAPID_KEY,
+    applicationServerKey: urlBase64ToUint8Array(
+      process.env.NEXT_PUBLIC_VAPID_KEY as string,
+    ),
   });
 
   await fetch(`${baseUrl}/notifications/subscribe`, {
     method: "POST",
+    credentials: "include",
     body: JSON.stringify({ ...subscription.toJSON(), email }),
     headers: { "Content-type": "application/json" },
   });
